e7efb9bced6dd08a4f18ba0f3ea87ff037d38ceb
Valery Semenenko <gearmobile@gmail.com>
2020-01-01 22:24:22 +0300

    feat: basic ui-router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// UI-ROUTER
+import { UIRouterModule } from '@uirouter/angular';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+// COMPONENTS
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+
+// STATES
+const homeState = { name: 'home', url: '/home', component: HomeComponent };
+const aboutState = { name: 'about', url: '/about', component: AboutComponent };
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HomeComponent, AboutComponent],
+  imports: [BrowserModule, UIRouterModule.forRoot({ states: [homeState, aboutState], useHash: true })],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .then(ref => {
+    if (window['ngRef']) {
+      window['ngRef'].destroy();
+    }
+    window['ngRef'] = ref;
+  })
+  .catch(error => console.error(error));
